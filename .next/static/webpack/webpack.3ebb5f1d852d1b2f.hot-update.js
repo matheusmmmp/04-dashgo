5366ac1c5b57d93e4f7e579d1a36733220f11f7b
Matheus <matheusmmmp@hotmail.com>
2021-12-28 12:53:56 -0300

    Second Commit - DashBoard

--- .next/static/webpack/webpack.3ebb5f1d852d1b2f.hot-update.js
+++ .next/static/webpack/webpack.3ebb5f1d852d1b2f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6b080657454299b0"; }
+/******/ 	__webpack_require__.h = function() { return "794fdd0dd43f6c77"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- .next/static/webpack/webpack.6d9714da34a9b7ff.hot-update.js
+++ .next/static/webpack/webpack.6d9714da34a9b7ff.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "4fe8028dbe902032"; }
+/******/ 	__webpack_require__.h = function() { return "105261d83211ff42"; }
 /******/ }();
 /******/ 
 /******/ }
