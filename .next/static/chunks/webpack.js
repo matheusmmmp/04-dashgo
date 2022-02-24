d2993127441fddfcbf12d3dafb16523176933c13
Matheus <matheusmmmp@hotmail.co>
2022-02-24 16:51:51 -0300

    Finish

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "47e58b82c49d448f"; }
+/******/ 		__webpack_require__.h = function() { return "7748c52dd6720609"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import Link from "next/link";
+import { useMutation } from 'react-query'
+
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Siderbar } from "../../components/Sidebar";
@@ -7,6 +9,9 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
     name: string;
@@ -23,13 +28,29 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+    const router = useRouter()
+    const createUser = useMutation(async(user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }           
+        })
+
+        return response.data.user;
+    },{
+        onSuccess: () => {
+            queryClient.invalidateQueries('users')
+        }
+    });
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
     })
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+       await createUser.mutateAsync(values);
        
-        console.log("TATA");
+       router.push('/users')
     }
 
     const { errors } = formState
